07557722836d1eaba700becaeb5e4a03fad2ad38
eloisaudias <95453556+eloisaudias@users.noreply.github.com>
2022-10-03 09:48:30 -0300

    Avance minimo en corroboracion

---
+++ Vehiculos/Control/ControlCorroborar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehiculos.Modelo;
+
+namespace Vehiculos.Control
+{
+    internal class ControlCorroborar
+    {
+        public Empleado CorroborarEmpleado(string nombre, string apellido, string DNI, string email, string telefono,
+                              string marca, string modelo, string anio, string patente)
+        {
+            if (nombre=="" || nombre.Any(char.IsDigit) || apellido=="" || apellido.Any(char.IsDigit) || !(DNI.Any(char.IsDigit)) ||
+                email=="" || !(email.Contains("@")) || telefono =="" || !(telefono.Any(char.IsDigit)) || marca=="" || modelo=="" ||
+                anio=="" || !(anio.Any(char.IsDigit)) || patente=="")
+            {
+                return null;
+            }
+            else
+            {
+                Empleado empleado = new Empleado();
+            }
+        }
+    }
+}
--- Vehiculos/Control/ControlEmpleados.cs
+++ Vehiculos/Control/ControlEmpleados.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehiculos.Data;
+using Vehiculos.Modelo;
 
 namespace Vehiculos.Control
 {
     internal class ControlEmpleados
     {
+        DataEmpleados dataEmpleado = new DataEmpleados();
+        ControlCorroborar controlCorroborar = new ControlCorroborar();
         public string AgregarEmpleado(string nombre, string apellido, string DNI, string email, string telefono,
                                       string marca, string modelo, string anio, string patente)
         {
-            if (CorroborarEmpleado(nombre,  apellido,  DNI,  email, telefono, marca, modelo, anio,  patente))
+            Empleado empleadoAux= controlCorroborar.CorroborarEmpleado(nombre, apellido, DNI, email, 
+                                                                       telefono, marca, modelo, anio, patente);
+            if (empleadoAux == null)
             {
-
+                return "Datos erroneos";
             }
-            return "";
-        }
-
-        public bool CorroborarEmpleado(string nombre, string apellido, string DNI, string email, string telefono,
-                                      string marca, string modelo, string anio, string patente)
-        {
-
+            else
+            {
+                dataEmpleado.Agregar(empleadoAux);
+                return "Empleado cargado con exito";
+            }
         }
 
     }
